b06725af4e67bb9fb81ca346a8f6b84290263a10
Akos Toth-Mate <akos@tothmate.com>
2020-10-31 14:01:13 +0100

    Initial Commit

---
+++ szpuki/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  szpuki
+//
+//  Created by Akos Toth-Mate on 2020. 10. 31..
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ szpuki/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  szpuki
+//
+//  Created by Akos Toth-Mate on 2020. 10. 31..
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
